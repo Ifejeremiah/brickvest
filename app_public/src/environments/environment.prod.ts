3c302a2c9a35f9608862b445fe268e492e74e4bf
Ife Jeremiah <jerryroboy16@gmail.com>
2023-02-24 15:02:19 +0100

    updates

--- app_public/src/environments/environment.prod.ts
+++ app_public/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  apiBaseUri: 'https://onebrick.herokuapp.com/api',
+  apiBaseUri: 'https://brickvest.onrender.com/api',
   // apiBaseUri: 'http://localhost:3000/api',
   flwPublicKey: 'FLWPUBK_TEST-af7f7973d38fff1f1b41bdd708da3254-X',
-  flwRedirectUri: 'https://onebrick.herokuapp.com/verify-transaction'
+  flwRedirectUri: 'https://brickvest.onrender.com/verify-transaction',
+  // flwRedirectUri: 'http://localhost:3000/verify-transaction'
 };
